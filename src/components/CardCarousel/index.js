4f1cf42ce3a0b25d8ddbf18cd38f0a7af10da8f3
Eric <ericgd@amazon.com>
2026-09-01 07:41:42 +0000

    init repo

--- src/components/CardCarousel/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './CardCarousel.jsx';
-export { default as CardCarousel } from './CardCarousel.jsx';
--- src/theme/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * 主题系统对外入口。
- * 集成/组件侧统一从此处消费：
- *   import { ThemeProvider, useTheme } from './theme';
- */
-export { ThemeProvider } from './ThemeProvider.jsx';
-export { useTheme } from './useTheme.js';
-export { ThemeContext } from './ThemeContext.js';
-export { getSystemTheme, subscribeSystemTheme } from './systemTheme.js';
